7ba5a10af5ddb736214d8b43f0f4865d78941dfa
Anitha123256 <anithaanu0463@gmail.comm>
2025-10-19 17:19:59 +0530

    add employee

--- tests/addemployee.spec.js
+++ tests/addemployee.spec.js
@@ -1,22 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-
-test('Verify employee Details ', async ({ page }) => {
-
-await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-await page.locator("//input[@placeholder='Username']").fill("Admin")
-await page.locator("//input[@placeholder='Password']").fill("admin123")
-await page.locator("//button[@type='submit']").click()
-await page.locator("(//li[@class='oxd-main-menu-item-wrapper']//a)[2]").click()
-await page.locator("//li[@class='oxd-topbar-body-nav-tab --visited']//a[1]").click()
-await page.locator("(//span[normalize-space(text())='Configuration']/following::input)[1]").fill("Ammu")
-await page.locator("(//span[normalize-space(text())='Configuration']/following::input)[3]").fill("Ani")
-await page.locator("(//label[normalize-space(text())='Employee Id']/following::input)[1]").fill("0521")
-await page.locator("//button[contains(.,'Save')]").click()
-await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/186")
-await expect (page.locator("//h6[text()='Personal Details']")).toBeVisible()
-
-
-
-})
- 
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'PIM' }).click();
+  await page.getByRole('link', { name: 'Add Employee' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).fill('Anu');
+  await page.getByRole('textbox', { name: 'Last Name' }).click();
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('E');
+  await page.getByRole('button', { name: 'Save' }).click();
+  await expect(page.getByRole('link', { name: 'Add Employee' })).toBeVisible();
+  
+});
