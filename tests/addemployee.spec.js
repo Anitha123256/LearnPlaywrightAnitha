adc84a6e6c9e69d5638e857e1a40c0c44ef01bb2
Anitha123256 <anithaanu0463@gmail.comm>
2025-10-19 17:52:58 +0530

    add

--- tests/addemployee.spec.js
+++ tests/addemployee.spec.js
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('verify add employee with mandatory fields', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'PIM' }).click();
-  await page.getByRole('link', { name: 'Add Employee' }).click();
+  await page.getByRole('listitem').filter({ hasText: 'Add Employee' }).click();
   await page.getByRole('textbox', { name: 'First Name' }).click();
   await page.getByRole('textbox', { name: 'First Name' }).fill('Anu');
   await page.getByRole('textbox', { name: 'Last Name' }).click();
   await page.getByRole('textbox', { name: 'Last Name' }).fill('E');
   await page.getByRole('button', { name: 'Save' }).click();
-  await expect(page.getByRole('link', { name: 'Add Employee' })).toBeVisible();
-  
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/209');
+
+  await expect(page.getByRole('heading', { name: 'Personal Details' })).toBeVisible();
 });
